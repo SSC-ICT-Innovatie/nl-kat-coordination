3727754870312ae4003c38db329d3cabbb10985b
Jan Klopper <jan@underdark.nl>
2026-02-25 10:41:50 +0100

    Fix import from types for tests

--- octopoes/tests/test_path.py
+++ octopoes/tests/test_path.py
@@ -11,7 +11,7 @@ from octopoes.models.path import (
 )
 from tests.mocks.mock_ooi_types import (
     ALL_OOI_TYPES,
-    ALL_TYPES_BY_NAME,
+    CONCRETE_OOITYPE_BY_NAME,
     MockDNSCNAMERecord,
     MockDNSZone,
     MockHostname,
@@ -25,7 +25,7 @@ from tests.mocks.mock_ooi_types import (
 
 
 @patch("octopoes.models.types.ALL_TYPES", ALL_OOI_TYPES)
-@patch("octopoes.models.types.ALL_TYPES_BY_NAME", ALL_TYPES_BY_NAME)
+@patch("octopoes.models.types.CONCRETE_OOITYPE_BY_NAME", CONCRETE_OOITYPE_BY_NAME)
 class PathTest(TestCase):
     def test_path_outoing_relation(self):
         path = Path.parse("MockResolvedHostname.hostname")
