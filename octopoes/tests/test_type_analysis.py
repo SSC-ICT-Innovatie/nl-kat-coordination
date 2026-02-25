9d98814b8916393fd0069f8a6a1f426b6607b44c
Jan Klopper <jan@underdark.nl>
2026-02-25 11:16:19 +0100

    Fix import in test

--- octopoes/tests/test_type_analysis.py
+++ octopoes/tests/test_type_analysis.py
@@ -11,7 +11,7 @@ from octopoes.models.pagination import Paginated
 from octopoes.models.types import get_collapsed_types, get_relations, to_concrete, type_by_name
 from tests.mocks.mock_ooi_types import (
     ALL_OOI_TYPES,
-    ALL_TYPES_BY_NAME,
+    CONCRETE_OOITYPE_BY_NAME,
     MockDNSCNAMERecord,
     MockDNSZone,
     MockHostname,
@@ -35,7 +35,7 @@ def get_abstract_types() -> set[type[OOI]]:
 
 
 @patch("octopoes.models.types.ALL_TYPES", ALL_OOI_TYPES)
-@patch("octopoes.models.types.ALL_TYPES_BY_NAME", ALL_TYPES_BY_NAME)
+@patch("octopoes.models.types.CONCRETE_OOITYPE_BY_NAME", CONCRETE_OOITYPE_BY_NAME)
 @patch("octopoes.models.types.get_concrete_types", get_concrete_types)
 @patch("octopoes.models.types.get_abstract_types", get_abstract_types)
 class TypeSystemTest(TestCase):
