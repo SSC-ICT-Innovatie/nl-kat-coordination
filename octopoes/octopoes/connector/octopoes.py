7504848d338d62a5beb8274007f95566d4f049fc
Jan Klopper <jan@underdark.nl>
2026-04-02 11:38:17 +0200

    fix typo

--- octopoes/octopoes/connector/octopoes.py
+++ octopoes/octopoes/connector/octopoes.py
@@ -422,7 +422,7 @@ class OctopoesAPIConnector:
     def load_objects_bulk(
         self, references: set[Reference], valid_time: datetime, with_scan_profiles: bool | None = True
     ) -> dict[Reference, OOIType]:
-        params = {"valid_time": str(valid_time), with_scan_profiles="true" if with_scan_profiles else "false"}
+        params = {"valid_time": str(valid_time), with_scan_profiles:"true" if with_scan_profiles else "false"}
         res = self.session.post(
             f"/{self.client}/objects/load_bulk", params=params, json=[str(ref) for ref in references]
         )
