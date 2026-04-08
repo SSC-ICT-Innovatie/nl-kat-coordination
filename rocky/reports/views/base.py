80b3e8cf7d5a9b0806201b8c03acf9d075621d61
Jan Klopper <janklopper+underdark@gmail.com>
2026-04-08 09:16:35 +0200

    Fix query to get OOI type names correctly

--- rocky/reports/views/base.py
+++ rocky/reports/views/base.py
@@ -275,7 +275,7 @@ class BaseReportView(OOIFilterView, ReportBreadcrumbs):
 
         if object_selection == "query":
             query = {
-                "ooi_types": [t.__name__ if isinstance(t, OOI) else t for t in self.get_ooi_types()],
+                "ooi_types": [t.__name__ for t in self.get_ooi_types()],
                 "scan_level": self.get_ooi_scan_levels(),
                 "scan_type": self.get_ooi_scan_profile_types(),
                 "search_string": self.search_string,
