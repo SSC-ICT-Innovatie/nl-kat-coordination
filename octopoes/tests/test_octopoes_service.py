aa84a6b957db5d14bb40a2ade9cf402e078b71b0
Jan Klopper <jan@underdark.nl>
2026-04-09 12:00:39 +0200

    use mocked OOI, thow now surfaces since inference now re-raises errors that where previously only logged

--- octopoes/tests/test_octopoes_service.py
+++ octopoes/tests/test_octopoes_service.py
@@ -97,7 +97,9 @@ def test_on_create_scan_profile(octopoes_service, new_data, old_data, bit_runner
         ]
     )
     octopoes_service.scan_profile_repository.get = MagicMock(return_value=Mock(level=ScanLevel.L2))
-    octopoes_service.ooi_repository.get = MagicMock(return_value=Mock())
+    octopoes_service.ooi_repository.get = MagicMock(
+        return_value=Hostname(hostname="test|source")
+    )
     octopoes_service.origin_parameter_repository.list_by_origin = MagicMock(return_value={})
     octopoes_service.ooi_repository.load_bulk = MagicMock(return_value={})
     octopoes_service.ooi_repository.save = MagicMock(return_value=True)
