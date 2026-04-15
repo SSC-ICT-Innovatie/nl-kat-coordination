61e240aa54538acfa97dcc96df218fca839ad97b
Jan Klopper <jan@underdark.nl>
2026-04-15 16:19:35 +0200

    forgot to import json

--- rocky/reports/views/mixins.py
+++ rocky/reports/views/mixins.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+import json
 
 import structlog
 from django.contrib import messages
--- rocky/rocky/views/task_detail.py
+++ rocky/rocky/views/task_detail.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
