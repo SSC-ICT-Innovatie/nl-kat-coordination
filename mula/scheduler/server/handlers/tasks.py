350d9d7f2aa2ee2bf4c88a38ca50b8045378d336
Jan Klopper <janklopper+underdark@gmail.com>
2026-06-05 09:44:11 +0200

    Update organisation_id to organisation_ids in stats method

--- mula/scheduler/server/handlers/tasks.py
+++ mula/scheduler/server/handlers/tasks.py
@@ -109,6 +109,6 @@ class TaskAPI:
         return updated_task
 
     def stats(
-        self, scheduler_id: str | None = None, organisation_id: str | None = None
+        self, scheduler_id: str | None = None, organisation_ids: list[str] | None = None
     ) -> dict[str, dict[str, int]] | None:
-        return self.ctx.datastores.task_store.get_status_count_per_hour(scheduler_id, organisation_id)
+        return self.ctx.datastores.task_store.get_status_count_per_hour(scheduler_id, organisation_ids)
