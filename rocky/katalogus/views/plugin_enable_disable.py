64a5c34574d729cd79ab1ab766723d312db728b1
Brenno de Winter <brenno@dewinter.com>
2026-08-09 22:53:12 +0200

    Add organization user activity logs

--- rocky/katalogus/views/plugin_enable_disable.py
+++ rocky/katalogus/views/plugin_enable_disable.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 import structlog
+from crisis_room.models import AuditLog
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls.base import reverse
@@ -22,6 +25,7 @@ class PluginEnableDisableView(SinglePluginView):
 
         if plugin_state == "True":
             self.katalogus_client.disable_plugin(self.plugin)
+            self.record_action(AuditLog.Action.PLUGIN_DISABLED)
             messages.add_message(
                 self.request,
                 messages.WARNING,
@@ -31,6 +35,7 @@ class PluginEnableDisableView(SinglePluginView):
 
         if self.plugin.can_scan(self.organization_member):
             self.katalogus_client.enable_plugin(self.plugin)
+            self.record_action(AuditLog.Action.PLUGIN_ENABLED)
             messages.add_message(
                 self.request, messages.SUCCESS, _("{} '{}' enabled.").format(self.plugin.type.title(), self.plugin.name)
             )
@@ -65,3 +70,16 @@ class PluginEnableDisableView(SinglePluginView):
         )
 
         return HttpResponseRedirect(redirect_url.split("#")[0])
+
+    def record_action(self, action: Any) -> None:
+        AuditLog.record(
+            user=self.request.user,
+            organization=self.organization,
+            action=action,
+            object_type=self.plugin.type.title(),
+            object_label=self.plugin.name,
+            object_url=reverse(
+                "boefje_detail" if self.plugin.type == "boefje" else "normalizer_detail",
+                kwargs={"organization_code": self.organization.code, "plugin_id": self.plugin.id},
+            ),
+        )
