c366a529c40cf740bd04e78741aa8311452627fb
Jan Klopper <janklopper+underdark@gmail.com>
2026-08-14 10:55:29 +0200

    Update module path for dns-identifiers BitDefinition

--- octopoes/bits/dns_identifiers/bit.py
+++ octopoes/bits/dns_identifiers/bit.py
@@ -2,5 +2,5 @@ from bits.definitions import BitDefinition
 from octopoes.models.ooi.dns.records import DNSTXTRecord
 
 BIT = BitDefinition(
-    id="dns-identifiers", consumes=DNSTXTRecord, parameters=[], module="bits.identifiers.identifiers", min_scan_level=1
+    id="dns-identifiers", consumes=DNSTXTRecord, parameters=[], module="bits.dns_identifiers.dns_identifiers", min_scan_level=1
 )
