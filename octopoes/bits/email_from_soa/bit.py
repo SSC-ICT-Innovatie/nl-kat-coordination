a80c27e3156caa0d5cf1eb89a7653ebd8e85423e
Jan Klopper <jan@underdark.nl>
2026-08-14 10:21:52 +0200

    fix bit param definition, and include emailaddress in test output

--- octopoes/bits/email_from_soa/bit.py
+++ octopoes/bits/email_from_soa/bit.py
@@ -4,7 +4,7 @@ from octopoes.models.ooi.dns.records import DNSSOARecord
 BIT = BitDefinition(
     id="email-from-soa",
     consumes=DNSSOARecord,
-    parameters=None,
+    parameters=[],
     module="bits.email_from_soa.email_from_soa",
     min_scan_level=0,
 )
--- octopoes/tests/test_ooi_repository.py
+++ octopoes/tests/test_ooi_repository.py
@@ -86,6 +86,7 @@ class OOIRepositoryTest(TestCase):
                             (pull ?e [
                                 :xt/id
                                 {:IPService/_ip_port [*]}
+                                {:EmailAddressInstance/_location [*]}
                                 {:Config/_ooi [*]}
                                 {:Finding/_ooi [*]}
                                 {:GeographicPoint/_ooi [*]}
