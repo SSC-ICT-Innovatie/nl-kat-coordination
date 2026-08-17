4a5fa7442b7883689162af05f8add8972894ed11
Jan Klopper <jan@underdark.nl>
2026-08-17 11:13:30 +0200

    Add extraction test

--- boefjes/tests/plugins/test_identifier.py
+++ boefjes/tests/plugins/test_identifier.py
@@ -1,5 +1,16 @@
+from boefjes.plugins.kat_webpage_analysis.find_identifiers_in_html.normalize import IdentifierMatch, extract_identifiers
 from boefjes.worker.job_models import NormalizerMeta
 from tests.loading import get_dummy_data
+
+
+def test_extract_identifiers():
+    raw = b"""<html><head><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-12345');</script></head></html>"""
+
+    assert extract_identifiers(raw.decode()) == {IdentifierMatch(vendor="GoogleTagManager", identifier="GTM-12345")}
 
 
 def test_find_identifiers_in_html(normalizer_runner):
