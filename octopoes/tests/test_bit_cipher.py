418e2a3bb153dd34c8f5bda4bd871b49ca734e69
stephanie0x00 <9821756+stephanie0x00@users.noreply.github.com>
2026-09-22 13:58:38 +0200

    fix cipher

--- octopoes/tests/test_bit_cipher.py
+++ octopoes/tests/test_bit_cipher.py
@@ -99,7 +99,7 @@ def test_medium_tls_ciphers():
     assert isinstance(finding, Finding)
     assert (
         finding.description == "One or more of the cipher suites should not be used because:\n"
-        "ECDHE-RSA-AES256-SHA384 - Using CBC as bulk encryption algorithm (Medium)."
+        "DHE-DSS-CAMELLIA128-SHA256 - Using CBC as bulk encryption algorithm (Medium)."
     )
 
 
